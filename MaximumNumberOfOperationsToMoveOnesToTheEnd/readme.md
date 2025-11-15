94c0ac64110d39da38937dbca983a66b4214a09c
mientran14 <155632908+mientran14@users.noreply.github.com>
2025-11-15 18:36:57 +0000

    leetcode problem: Maximum Number Of Operations To Move Ones To The End

---
+++ MaximumNumberOfOperationsToMoveOnesToTheEnd/readme.md
@@ -0,0 +1,13 @@
+## Problem
+
+https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/description/
+
+```
+```
+
+## Observations
+
+## Solution
+
+# Tags
+
---
+++ MaximumNumberOfOperationsToMoveOnesToTheEnd/solve.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxOperations(string s) {
+        int result = 0;
+        int ones = 0;
+        for (int i=0; i<s.size(); i++) {
+            if (s[i]=='1') {
+                ones++;
+            }
+            else if (i>0 && s[i-1]=='1') {
+                result+=ones;
+            }
+        }
+        return result;
+    }
+};
