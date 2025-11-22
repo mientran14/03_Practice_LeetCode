6b9456500af14f444771066ac687efdc5d6e4fa6
mientran14 <155632908+mientran14@users.noreply.github.com>
2025-11-22 14:49:22 +0000

    leetcode problem: Find Minimum Operations To Make All Elements Divisible By Three

--- FindMinimumOperationsToMakeAllElementsDivisibleByThree/solve.cpp
+++ FindMinimumOperationsToMakeAllElementsDivisibleByThree/solve.cpp
@@ -2,10 +2,16 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
         int result = 0;
+
         for (int x : nums) {
             int r = x % 3;
-            if (r == 1 || r == 2) {
-                result++;
+
+            // if (r == 1 || r == 2) {
+            //     result++;
+            // }
+
+            if (r != 0) {
+                result += min(r, 3 - r);
             }
         }
         return result;
