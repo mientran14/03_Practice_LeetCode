55e326f281fe87e815ef627acca268867bf0ff8e
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2025-12-27 22:55:47 +0700

    leetcode problem: Sort Array By Parity

--- SortArrayByParity/solve.cpp
+++ SortArrayByParity/solve.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
+        // vector<int> even;
+        // vector<int> odd;
+        // for (int n : nums) {
+        //     if (n%2 == 0) even.push_back(n);
+        //     else odd.push_back(n);
+        // }
+        // even.insert(even.end(), odd.begin(), odd.end());
+        // return even;
+
+
+        // int left = 0, right = nums.size() - 1;
+        // while (left < right) {
+        //     if (nums[left] % 2 == 0) {
+        //         left++;
+        //     }
+        //     else if (nums[right] % 2 != 0) {
+        //         right--;
+        //     }
+        //     else {
+        //         swap(nums[left], nums[right]);
+        //         left++;
+        //         right--;
+        //     }
+        // }
+        // return nums;
+
         int indexEven = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i]%2 == 0) swap(nums[indexEven++], nums[i]);
