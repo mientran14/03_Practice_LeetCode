eb323af2a9d059f46eec8ea105077a94c75f1422
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-01-07 23:41:42 +0700

    leetcode problem: Add To Array Form Of Integer

--- AddToArrayFormOfInteger/solve.cpp
+++ AddToArrayFormOfInteger/solve.cpp
@@ -1,1 +1,20 @@
-// Your code here
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int> result;
+        int carry = k;
+
+        for (int i = num.size()-1; i >= 0; i--) {
+            int sum = num[i] + carry;
+            result.push_back(sum%10);
+            carry = sum/10;
+        }
+        while (carry > 0) {
+            result.push_back(carry%10);
+            carry /= 10;
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
