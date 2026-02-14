25f7499c7d27c87c4da6649da65418166323c3db
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-02-15 00:38:33 +0700

    leetcode problem: Concatenation Of Array

--- ConcatenationOfArray/solve.cpp
+++ ConcatenationOfArray/solve.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n*2);
-        for (int i = 0; i < n; i++) {
-            ans[i] = nums[i];
-            ans[i+n] = nums[i];
-        }
-        return ans;
+        // int n = nums.size();
+        // vector<int> ans(n*2);
+        // for (int i = 0; i < n; i++) {
+        //     ans[i] = nums[i];
+        //     ans[i+n] = nums[i];
+        // }
+        // return ans;
+
+        nums.insert(nums.end(), nums.begin(), nums.end());
+        return nums;
     }
 };
