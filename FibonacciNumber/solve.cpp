41ff16074fcda4002523c65f091196dd277e7153
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-03-07 22:19:46 +0700

    leetcode problem: Fibonacci Number

--- FibonacciNumber/solve.cpp
+++ FibonacciNumber/solve.cpp
@@ -6,15 +6,26 @@ public:
         // if (n == 1) return 1;
         // return fib(n-1) + fib(n-2);
 
-        // DP
+        // // DP
+        // if (n == 0) return 0;
+        // if (n == 1) return 1;
+        // vector<int> dp(n+1, 0);
+        // dp[0] = 0;
+        // dp[1] = 1;
+        // for (int i = 2; i <= n; i++) {
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+
+        // bottom‑up (refer to climbStairs)
         if (n == 0) return 0;
         if (n == 1) return 1;
-        vector<int> dp(n+1, 0);
-        dp[0] = 0;
-        dp[1] = 1;
+        int a = 0, b = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            int c = a + b;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        return b;
     }
 };
