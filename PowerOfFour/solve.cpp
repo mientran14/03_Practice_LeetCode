7b125b93ee4684df919bb5f7c3f735339d7f2349
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-03-07 21:54:58 +0700

    leetcode problem: Power Of Four

--- PowerOfFour/solve.cpp
+++ PowerOfFour/solve.cpp
@@ -6,9 +6,11 @@ public:
         // while (n % 4 == 0) n /= 4;
         // return n == 1;
 
-        // #2
-        if (n <= 0) return false;
-        if (n == 1) return true;
-        return n % 4 == 0 && isPowerOfFour(n / 4);
+        // // #2
+        // if (n <= 0) return false;
+        // if (n == 1) return true;
+        // return n % 4 == 0 && isPowerOfFour(n / 4);
+
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
     }
 };
