4e256eecdbf1bef64f8a3c6e3320d1fe7f3d860f
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-03-14 20:12:54 +0700

    leetcode problem: Count Pairs That Form A Complete Day I

--- CountPairsThatFormACompleteDayI/solve.cpp
+++ CountPairsThatFormACompleteDayI/solve.cpp
@@ -1,11 +1,21 @@
 class Solution {
 public:
     int countCompleteDayPairs(vector<int>& hours) {
+        // int result = 0;
+        // for (int i = 0; i < hours.size(); i++) {
+        //     for (int j = i+1; j < hours.size(); j++) {
+        //         if (i != j && (hours[i] + hours[j]) % 24 == 0) result++;
+        //     }
+        // }
+        // return result;
+
         int result = 0;
-        for (int i = 0; i < hours.size(); i++) {
-            for (int j = i+1; j < hours.size(); j++) {
-                if (i != j && (hours[i] + hours[j]) % 24 == 0) result++;
-            }
+        unordered_map<int, int> freq;
+        for (int h : hours) {
+            int complement = (24-(h%24) + 24) % 24;
+            result += freq[complement];
+            int remainder = (h%24 + 24) % 24;
+            freq[remainder]++;
         }
         return result;
     }
