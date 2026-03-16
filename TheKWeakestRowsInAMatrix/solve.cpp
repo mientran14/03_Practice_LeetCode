39e17642d5a504412e1b2d80993dddd80228876a
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-03-17 00:34:35 +0700

    leetcode problem: Minimum Number Of Seconds To Make Mountain Height Zero

--- TheKWeakestRowsInAMatrix/solve.cpp
+++ TheKWeakestRowsInAMatrix/solve.cpp
@@ -1,24 +1,61 @@
 class Solution {
 public:
-    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-        
-        int m = mat.size(), n = mat[0].size();
-        vector<int> result;
-        
-        for (int i = 0; i < m; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1) count++;
-                else break;
-            }
-            minHeap.push({count, i});
+    // long long maxHeightReduced(long long Tmax, int t) {
+    //     long long maxHeight = 0;
+    //     long long left = 0, right = 1e6;                // 1 <= mountainHeight <= 105
+
+    //     while (left <= right) {
+    //         long long mid = left + (right - left) / 2;
+    //         long long reqTime = t * (mid*(mid+1)/2);    // = t * (1+2+...+mid)
+
+    //         if (reqTime <= Tmax) {
+    //             maxHeight = mid;
+    //             left = mid + 1;
+    //         } 
+    //         else right = mid - 1;
+    //     }
+    //     return maxHeight;
+    // }
+
+    // long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+    //     long long left = 1, right = 1e18;
+    //     long long result = right;
+
+    //     while (left <= right) {
+    //         long long mid = (left + right) / 2;
+    //         long long reduced = 0;
+
+    //         for (int t : workerTimes) {
+    //             reduced += maxHeightReduced(mid, t);
+    //             if (reduced >= mountainHeight) {
+    //                 result = mid;
+    //                 right = mid - 1;
+    //                 break;
+    //             }
+    //         }
+            
+    //         if (reduced < mountainHeight) left = mid + 1;
+    //     }
+    //     return result;
+
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        priority_queue<tuple<long long, long long, long long>, vector<tuple<long long, long long, long long>>, greater<>> minHeap;
+
+        for (int t : workerTimes) {
+            minHeap.push({t, t, 1});
         }
-        
-        for (int i = 0; i < k; i++) {
-            result.push_back(minHeap.top().second);
+
+        long long totalTime = 0;
+        while (mountainHeight > 0) {
+            auto [currTime, initTime, count] = minHeap.top();
             minHeap.pop();
+            
+            totalTime = max(totalTime, currTime);
+            count++;
+            currTime += initTime * count;
+            minHeap.push({currTime, initTime, count});
+            mountainHeight--;
         }
-        return result;
+        return totalTime;
     }
 };
