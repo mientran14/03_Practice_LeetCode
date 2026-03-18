75304380b95721736b9cba33743ba070689da933
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-03-18 23:38:01 +0700

    leetcode problem: Count Submatrices With Top Left Element And Sum Less Than K

---
+++ CountSubmatricesWithTopLeftElementAndSumLessThanK/readme.md
@@ -0,0 +1,13 @@
+## Problem
+
+https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/description
+
+```
+```
+
+## Observations
+
+## Solution
+
+# Tags
+
---
+++ CountSubmatricesWithTopLeftElementAndSumLessThanK/solve.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int m = grid.size(); 
+        int n = grid[0].size();
+        int result = 0;
+
+        //  // TLE 854 / 859 testcases passed
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         int sum = 0;
+        //         for (int x = 0; x <= i; x++) {
+        //             for (int y = 0; y <= j; y++) {
+        //                 sum += grid[x][y];
+        //             }
+        //         }
+        //         if (sum <= k) result++;
+        //     }
+        // }
+        // return result;
+
+        // prefix sum (quên mấy lần rồi ?)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) grid[i][j] += grid[i-1][j];
+                if (j > 0) grid[i][j] += grid[i][j-1];
+                if (i > 0 && j > 0) grid[i][j] -= grid[i-1][j-1];
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] <= k) result++; 
+            }
+        }
+        
+        return result;
+    }
+};
