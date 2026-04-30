b34b7c52bea306d6f09a52b298d019ab729a6217
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-04-30 23:32:54 +0700

    leetcode problem: Maximum Path Score In A Grid

---
+++ MaximumPathScoreInAGrid/readme.md
@@ -0,0 +1,13 @@
+## Problem
+
+https://leetcode.com/problems/maximum-path-score-in-a-grid/description
+
+```
+```
+
+## Observations
+
+## Solution
+
+# Tags
+
---
+++ MaximumPathScoreInAGrid/solve.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int maxPathScore(vector<vector<int>>& grid, int k) {
+
+        // Example 1: grid = [[0, 1],[2, 0]], k = 1
+        //     (0,0) -> (0,1)
+        //       ↓        ↓
+        //     (1,0) -> (1,1)
+        //         (0,0): score = 0, costs = 0
+        //         (0,1): score = 1, costs = 1
+        //         (1,0): score = 2, costs = 1
+        //         (1,1): score = 0, costs = 0
+        //     maxScore((0,0)->(0,1)->(1,1), (0,0)->(1,0)->(1,1))
+
+
+        // // TLE 4/529 testcases passed
+        // int m = grid.size(), n = grid[0].size();
+        // vector<pair<int, int>> dirs = {{0, 1}, {1, 0}};
+        // function<int(int, int, int)> dfs = [&](int i, int j, int cost) {
+        //     if (cost > k) return -1;
+        //     if (i == m-1 && j == n-1) return grid[i][j];
+        //     int maxScore = -1;
+        //     for (auto [di, dj] : dirs) {
+        //         int ni = i+di, nj = j+dj;
+        //         if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+        //             int newCost = cost + (grid[ni][nj] > 0 ? 1 : 0);
+        //             int score = dfs(ni, nj, newCost);
+        //             if (score != -1) {
+        //                 maxScore = max(maxScore, score + grid[i][j]);
+        //             }
+        //         }
+        //     }
+        //     return maxScore;
+        // };
+        // return dfs(0, 0, 0);
+        
+
+
+        // Using Memoization -> TLE 249/529 testcases passed
+        int m = grid.size(), n = grid[0].size();
+        vector<pair<int, int>> dirs = {{0, 1}, {1, 0}};
+        // memo[i][j][cost]
+        vector<vector<vector<int>>> memo(m, vector<vector<int>>(n, vector<int>(k+1, -1)));
+
+        function<int(int, int, int)> dfs = [&](int i, int j, int cost) {
+            if (cost > k) return -1;
+            if (i == m-1 && j == n-1) return grid[i][j];
+
+            if (memo[i][j][cost] != -1) return memo[i][j][cost];
+
+            int maxScore = -1;
+            for (auto [di, dj] : dirs) {
+                int ni = i+di, nj = j+dj;
+                if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                    int newCost = cost + (grid[ni][nj] > 0 ? 1 : 0);
+                    int score = dfs(ni, nj, newCost);
+                    if (score != -1) {
+                        maxScore = max(maxScore, score + grid[i][j]);
+                    }
+                }
+            }
+
+            return memo[i][j][cost] = maxScore;
+        };
+        return dfs(0, 0, 0);
+
+        // Hint: using Tabulation -> Need to improve
+    }
+};
