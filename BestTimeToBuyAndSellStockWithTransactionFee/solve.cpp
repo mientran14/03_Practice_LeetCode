d6602a60bb4697c2aa8f219cfe96cc38aaa674dc
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-06-07 13:01:58 +0700

    leetcode problem: Best Time To Buy And Sell Stock With Transaction Fee

---
+++ BestTimeToBuyAndSellStockWithTransactionFee/solve.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int sell = 0;
+        int hold = -prices[0];
+        
+        for (int i = 1; i < prices.size(); i++) {
+            sell = max(sell, hold + prices[i] - fee);
+            hold = max(hold, sell - prices[i]);
+        }
+        
+        return sell;
+    }
+};
