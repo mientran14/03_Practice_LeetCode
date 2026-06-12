15b94c0d15c4538807daa4dee493a25d2e73186e
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-06-12 22:40:46 +0700

    leetcode problem: Sort Colors

---
+++ SortColors/solve.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        
+        for (int i = 0; i <= right; i++) {
+            if (nums[i] == 0) {
+                swap(nums[i], nums[left]);
+                left++;
+            } 
+            else if (nums[i] == 2) {
+                swap(nums[i], nums[right]);
+                right--;
+                i--; // Check lại ptử vừa được swap
+            }
+        }
+    }
+};
