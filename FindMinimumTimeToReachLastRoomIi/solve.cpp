2d4c8c83b8953164670243a515a2e8174319ef0e
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-07-11 23:18:53 +0700

    leetcode problem: Find Minimum Time To Reach Last Room Ii

--- FindMinimumTimeToReachLastRoomIi/solve.cpp
+++ FindMinimumTimeToReachLastRoomIi/solve.cpp
@@ -4,11 +4,11 @@ public:
         int n = moveTime.size();
         int m = moveTime[0].size();
         
-        vector<vector<vector<int>>> dist(n, vector<vector<int>>(m, vector<int>(2, INT_MAX)));
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
         
-        minHeap.push({0, 0, 0, 0}); // {time, x, y, state}
-        dist[0][0][0] = 0;
+        minHeap.push({0, 0, 0});
+        dist[0][0] = 0;
         
         vector<pair<int, int>> direc = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         
@@ -19,23 +19,22 @@ public:
             int curTime = curr[0];
             int x = curr[1];
             int y = curr[2];
-            int state = curr[3];
             
             if (x == n-1 && y == m-1) return curTime;
-            if (curTime > dist[x][y][state]) continue;
+            if (curTime > dist[x][y]) continue;
+
+            int stepTime = ((x + y) % 2) + 1;
 
             for (auto [dx, dy] : direc) {
                 int nx = x + dx;
                 int ny = y + dy;
                 
                 if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    int stepTime = (state == 0) ? 1 : 2;
                     int nextTime = max(curTime, moveTime[nx][ny]) + stepTime;
-                    int nextState = state^1; // nhớ đảo state lại
 
-                    if (nextTime < dist[nx][ny][nextState]) {
-                        dist[nx][ny][nextState] = nextTime;
-                        minHeap.push({nextTime, nx, ny, nextState});
+                    if (nextTime < dist[nx][ny]) {
+                        dist[nx][ny] = nextTime;
+                        minHeap.push({nextTime, nx, ny});
                     }
                 }
             }
