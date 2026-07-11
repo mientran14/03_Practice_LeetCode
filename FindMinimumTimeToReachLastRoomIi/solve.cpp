27de495d77aa9eb333f11e49d1555d85ccc8a7fd
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-07-11 18:58:21 +0700

    leetcode problem: Find Minimum Time To Reach Last Room Ii

--- FindMinimumTimeToReachLastRoomIi/solve.cpp
+++ FindMinimumTimeToReachLastRoomIi/solve.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        // Wrong Answer 105 / 717 testcases passed
-
         int n = moveTime.size();
         int m = moveTime[0].size();
-        vector<vector<int>> visited(n, vector<int>(m, INT_MAX));
-        queue<pair<int, pair<int, int>>> q;
-
-        vector<pair<int, int>> direc = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-        q.push({0, {0, 0}});
-        visited[0][0] = 0;
         
-        while (!q.empty()) {
-            auto [curTime, pos] = q.front();
-            int x = pos.first;
-            int y = pos.second;
-            q.pop();
+        vector<vector<vector<int>>> dist(n, vector<vector<int>>(m, vector<int>(2, INT_MAX)));
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
+        
+        minHeap.push({0, 0, 0, 0}); // {time, x, y, state}
+        dist[0][0][0] = 0;
+        
+        vector<pair<int, int>> direc = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        
+        while (!minHeap.empty()) {
+            auto curr = minHeap.top();
+            minHeap.pop();
+            
+            int curTime = curr[0];
+            int x = curr[1];
+            int y = curr[2];
+            int state = curr[3];
             
             if (x == n-1 && y == m-1) return curTime;
+            if (curTime > dist[x][y][state]) continue;
 
             for (auto [dx, dy] : direc) {
                 int nx = x + dx;
                 int ny = y + dy;
+                
                 if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    int nextTime = curTime + 1;
-                    if ((curTime + 1) % 2 == 0) nextTime++;
-                    
-                    if (nextTime < moveTime[nx][ny]) {
-                        nextTime = moveTime[nx][ny];
-                        if ((nextTime - curTime) % 2 == 0) nextTime++;
-                    }
-                    
-                    if (nextTime < visited[nx][ny]) {
-                        visited[nx][ny] = nextTime;
-                        q.push({nextTime, {nx, ny}});
+                    int stepTime = (state == 0) ? 1 : 2;
+                    int nextTime = max(curTime, moveTime[nx][ny]) + stepTime;
+                    int nextState = state^1; // nhớ đảo state lại
+
+                    if (nextTime < dist[nx][ny][nextState]) {
+                        dist[nx][ny][nextState] = nextTime;
+                        minHeap.push({nextTime, nx, ny, nextState});
                     }
                 }
             }
