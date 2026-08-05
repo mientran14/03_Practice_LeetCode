7fb4aacda780b658644569eaa3e3c7db2b907545
MIEN THI XUAN TRAN/LGEDV FUNCTION TECHNOLOGY TEAM <mien.tran@lge.com>
2026-08-05 12:15:31 +0700

    leetcode problem: Remove Methods From Project

---
+++ RemoveMethodsFromProject/solve.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        // Time Limit Exceeded 774 / 775 testcases passed
+        
+        vector<bool> isSuspicious(n, false);
+        isSuspicious[k] = true;
+
+        // Nếu a suspicious thì b cũng suspicious
+        bool flag = true;
+        while (flag) {
+            flag = false;
+            for (auto& inv : invocations) {
+                int a = inv[0];
+                int b = inv[1];
+                if (isSuspicious[a] && !isSuspicious[b]) {
+                    isSuspicious[b] = true;
+                    flag = true;
+                }
+            }
+        }
+
+        for (auto& inv : invocations) {
+            int a = inv[0];
+            int b = inv[1];
+            // If it is not possible to remove all the suspicious methods, none should be removed.
+            if (!isSuspicious[a] && isSuspicious[b]) {
+                vector<int> all(n);
+                for (int i = 0; i < n; ++i) all[i] = i;
+                return all;
+            }
+        }
+
+        vector<int> remain;
+        for (int i = 0; i < n; ++i) {
+            if (!isSuspicious[i]) {
+                remain.push_back(i);
+            }
+        }
+
+        return remain;
+    }
+};
